6aedeb7f97405d5d6417c77d63f6b453fb160480
Jakub <47848941+japolak@users.noreply.github.com>
2022-04-27 10:34:58 +0200

    Create modelhelpers.py

---
+++ scripts/modules/modelhelpers.py
@@ -0,0 +1,18 @@
+def vip(x, y, model):
+    t = model.x_scores_
+    w = model.x_weights_
+    q = model.y_loadings_
+
+    m, p = x.shape
+    _, h = t.shape
+
+    vips = np.zeros((p,))
+
+    s = np.diag(t.T @ t @ q.T @ q).reshape(h, -1)
+    total_s = np.sum(s)
+
+    for i in range(p):
+        weight = np.array([ (w[i,j] / np.linalg.norm(w[:,j]))**2 for j in range(h) ])
+        vips[i] = np.sqrt(p*(s.T @ weight)/total_s)
+
+    return vips
